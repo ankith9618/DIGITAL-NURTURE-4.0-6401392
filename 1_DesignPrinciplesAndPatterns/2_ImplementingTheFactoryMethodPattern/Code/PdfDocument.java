277618e70bf9cf012585283a2eccb5fb91e0c0b2
ankith9618 <122508376+ankith9618@users.noreply.github.com>
2025-06-21 23:10:08 +0530

    Delete 1_DesignPrinciplesAndPatterns directory

--- 1_DesignPrinciplesAndPatterns/2_ImplementingTheFactoryMethodPattern/Code/PdfDocument.java
+++
@@ -1,9 +0,0 @@
-public class PdfDocument implements Document {
-    public PdfDocument(){
-        System.out.println("\n--- PdfDocument created ---");
-    }
-    public void open() {
-        System.out.println("PDF Document Opened");
-    }
-}
-
--- 1_DesignPrinciplesAndPatterns/2_ImplementingTheFactoryMethodPattern/Code/WordDocument.java
+++
@@ -1,9 +0,0 @@
-public class WordDocument implements Document {
-    public WordDocument(){
-        System.out.println("\n--- WordDocument created ---");
-    }
-
-    public void open() {
-        System.out.println("Word Document Opened");
-    }
-}
