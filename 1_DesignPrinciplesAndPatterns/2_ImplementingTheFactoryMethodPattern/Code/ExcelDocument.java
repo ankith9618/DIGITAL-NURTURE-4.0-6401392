79657d9e450600693752f80e0d3ecd8546bef040
ankith9618 <122508376+ankith9618@users.noreply.github.com>
2025-06-21 23:05:07 +0530

    Adding week1 hands-on DesignPrinciplesAndPatterns

---
+++ 1_DesignPrinciplesAndPatterns/2_ImplementingTheFactoryMethodPattern/Code/ExcelDocument.java
@@ -0,0 +1,10 @@
+public class ExcelDocument implements Document {
+
+    public ExcelDocument(){
+        System.out.println("\n--- ExcelDocument created ---");
+    }
+    public void open() {
+        System.out.println("Excel Document Opened");
+    }
+}
+
