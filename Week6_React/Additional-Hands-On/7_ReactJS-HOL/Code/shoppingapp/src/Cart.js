23d9f6170d8f7f0e3768fa703fa81634dec71ee4
ankith9618 <ankithkumar9618@gmail.com>
2025-07-27 23:23:37 +0530

    7_ReactJS-HOL

---
+++ Week6_React/Additional-Hands-On/7_ReactJS-HOL/Code/shoppingapp/src/Cart.js
@@ -0,0 +1,29 @@
+// Cart.js
+import React, { Component } from 'react';
+
+class Cart extends Component {
+  render() {
+    return (
+      <table border="1" align="center" cellPadding="10" cellSpacing="7" style={{ color: 'green', fontWeight: 'bold' }}>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.props.item.map((item, index) => {
+            return (
+              <tr key={index}>
+                <td>{item.itemname}</td>
+                <td>{item.price}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    );
+  }
+}
+
+export default Cart;
